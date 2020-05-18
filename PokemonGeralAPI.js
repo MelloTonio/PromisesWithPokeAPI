0d3d0e9e99704920221a4c765cd9fd0025c94050
MelloTonio <60418444+MelloTonio@users.noreply.github.com>
2020-05-17 23:34:43 -0300

    Update PokemonGeralAPI.js

--- PokemonGeralAPI.js
+++ PokemonGeralAPI.js
@@ -8,6 +8,27 @@ async function obterPokemons(numero){
     return response.data
 }
 
-module.exports = {
-    obterPokemons
+async function main(){
+    try{
+        let pokeNames = []
+        const result = await obterPokemons('50')
+/*      for(let i = 0; i <= result.results.length-1; i++){
+            let pokemon = result.results[i]
+            pokeNames.push(pokemon.name)
+        } */
+/*         for(let i in result.results){
+            pokemon = (result.results[i])
+            pokeNames.push(pokemon.name)
+        }
+ */
+        for(pokemon of result.results){
+            pokeNames.push(pokemon.name)
+        } 
+        console.log(pokeNames)
+    }
+    catch(error){
+        console.error(error)
+    }
+    
 }
+main()
